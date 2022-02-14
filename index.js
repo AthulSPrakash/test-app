b930b91c6e5b336d932f4aa9c06b79c6ed69e5f8
Athul <athulsprakash@gmail.com>
2022-02-14 17:43:03 +0530

    initial commit

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ dotenv.config()
 
 app.use(cors())
 
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    next()
+})
+
 const port = process.env.PORT || 5000
 const username = process.env.USER_NAME
 const password = process.env.PASSWORD
