42f155a276375f560e497daa77208c18fc27a9a0
Athul <athulsprakash@gmail.com>
2022-02-14 16:34:51 +0530

    initial commit

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ const corsOption = {
 
 app.use(cors(corsOption))
 
-// app.use(function(req, res, next){
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     next()
-// })
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+})
 
 const dotenv = require('dotenv')
 dotenv.config()
