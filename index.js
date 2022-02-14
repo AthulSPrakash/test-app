41828dccb9e6d165fc6839e3a5e6c5f83a521582
Athul <athulsprakash@gmail.com>
2022-02-14 15:46:14 +0530

    initial commit

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ mongoose.connect(url,
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use('/api/gauth', Google)
 app.use('/api/login', Login)
 app.use('/api/register', Register)
