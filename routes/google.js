cb057803945b1a6f58eb96a51d5d8ced6ef85976
Athul <athulsprakash@gmail.com>
2022-02-14 17:54:02 +0530

    initial commit

--- routes/google.js
+++ routes/google.js
@@ -7,13 +7,6 @@ const JWT = require('jsonwebtoken')
 const dotenv = require('dotenv')
 dotenv.config()
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    next()
-})
-
 app.post('/', async (req,res)=>{
 
     const user = await googleAuth(req.body.token)
