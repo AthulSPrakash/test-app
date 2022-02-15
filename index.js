274f6a0865aa47d07cd076b54c505e3209658319
Athul <athulsprakash@gmail.com>
2022-02-15 14:05:01 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const DBConnect = require('./mongo')
 const Login = require('./routes/login')
 const Google = require('./routes/google')
 const Register = require('./routes/register')
 const SaveData = require('./routes/saveData')
+const port = process.env.PORT || 5000
+
 const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express()
-
-dotenv.config()
 
 const corsOptions = {
     origin: '*',
     credentials: true,
     optionSuccessStatus: 200
 }
-
 app.use(cors(corsOptions))
-
-const port = process.env.PORT || 5000
-const username = process.env.USER_NAME
-const password = process.env.PASSWORD
-// const cluster = process.env.CLUSTER
-const dbname = process.env.DB_NAME
-const url = `mongodb://${username}:${password}@cluster0-shard-00-00.pqq7v.mongodb.net:27017,cluster0-shard-00-01.pqq7v.mongodb.net:27017,cluster0-shard-00-02.pqq7v.mongodb.net:27017/${dbname}?ssl=true&replicaSet=atlas-qwfgu4-shard-0&authSource=admin&retryWrites=true&w=majority`
-const mongooseOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
-
-const connectDB = async () => {
-    await mongoose.connect(url, mongooseOptions)
-    .then(()=>console.log('Connected to MongoDB'))
-    .catch(err=>{console.log('Connection err:', err)})
-}
-connectDB()
-
 app.use(express.json())
+
+DBConnect().then(async mongoose =>{
+    try{
+        console.log('DB Connection successfull!!');
+    }finally{
+        mongoose.connection.close()
+    }
+})
 
 app.get('/', (req,res)=>{ res.json('Server running')})
 app.use('/api/gauth', Google)
