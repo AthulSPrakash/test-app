74e5863d5c21b9ce06555dd6968ae77fd6b38c11
Athul <athulsprakash@gmail.com>
2022-02-15 13:29:54 +0530

    initial commit

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use(cors(corsOptions))
 const port = process.env.PORT || 5000
 const username = process.env.USER_NAME
 const password = process.env.PASSWORD
-const cluster = process.env.CLUSTER
+// const cluster = process.env.CLUSTER
 const dbname = process.env.DB_NAME
-const url = `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`
+const url = `mongodb://${username}:${password}@cluster0-shard-00-00.pqq7v.mongodb.net:27017,cluster0-shard-00-01.pqq7v.mongodb.net:27017,cluster0-shard-00-02.pqq7v.mongodb.net:27017/${dbname}?ssl=true&replicaSet=atlas-qwfgu4-shard-0&authSource=admin&retryWrites=true&w=majority`
 const mongooseOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -33,17 +33,12 @@ const mongooseOptions = {
 const connectDB = async () => {
     try{
         await mongoose.connect(url, mongooseOptions)
+        console.log('Connected to MongoDB')
     }catch (err) {
         console.log('Error: ' + err)
-        throw err
     }
 }
-
-connectDB().then(()=>{
-    console.log('Connected to MongoDB')
-}).catch(err=>{
-    console.log('Error :', err)
-})
+connectDB()
 
 app.use(express.json())
 
